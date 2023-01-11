7032d236e4883cd15fe286382227c9a139d42235
pws0601 <ws0601@naver.com>
2023-01-11 18:07:55 +0900

    회사 작업 커밋

--- MongoDB/userDic/UserDicDistribute.py
+++ MongoDB/userDic/UserDicDistribute.py
@@ -121,7 +121,7 @@ import datetime
 
 # redis 연결
 
-rd = redis.StrictRedis(host='172.17.0.4', port=6379, db=0)
+rd = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 rd.set("userDict",res,datetime.timedelta(seconds=60))
 
