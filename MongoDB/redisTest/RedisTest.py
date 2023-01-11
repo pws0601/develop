c73d443dc12d74802b4c6fd6644fbf9c1e48647b
pws0601 <ws0601@naver.com>
2023-01-11 11:00:41 +0900

    회사 개발 컨테이너 새로 셋팅

--- MongoDB/redisTest/RedisTest.py
+++ MongoDB/redisTest/RedisTest.py
@@ -1,7 +1,7 @@
 import redis
 
 # redis 연결
-rd = redis.StrictRedis(host='172.17.0.4', port=6379, db=0)
+rd = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 #rd.set("test_key","test_value")
 
--- MongoDB/redisTest/RedisTestGet.py
+++ MongoDB/redisTest/RedisTestGet.py
@@ -2,7 +2,7 @@ import redis
 import json
 
 # redis 연결
-rd = redis.StrictRedis(host='172.17.0.4', port=6379, db=0)
+rd = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 byteUserDict = rd.get("userDict")
 userDictList = byteUserDict.decode('utf-8')
