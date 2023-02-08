ad206c3b0e4090022c56f00993768c8b3c62ccce
pws0601 <ws0601@naver.com>
2023-02-09 00:46:53 +0900

    20230209 python 스터디코드

--- CodingTest/03_탐색_시뮬레이션/02_숫자만추출/02_숫자만추출.py
+++ CodingTest/03_탐색_시뮬레이션/02_숫자만추출/02_숫자만추출.py
@@ -5,6 +5,7 @@ print(path)
 import sys
 sys.stdin=open(path+"/input.txt", "rt")
 
+# 숫자만 추출
 s = input()
 res = 0
 for x in s:
@@ -12,6 +13,9 @@ for x in s:
         res=res*10+int(x)
 
 print(res)
+
+# 추출된 숫자의 약수 구하기
+# 약수는 1부터 숫자만큼 1씩 커지면서 나눈 나머지가 0일경우 약수
 cnt=0
 for i in range(1, res+1):
     if res%i==0:
