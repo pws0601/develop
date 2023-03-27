a3f64ed7a609f6aac00b28413eb761f275b6b561
pws0601 <ws0601@naver.com>
2023-03-28 00:03:58 +0900

    20230328 python 스터디코드

---
+++ CodingTest/07_깊이_넓이우선탐색_DFS_BFS_활용/13_섬나라아일랜드_BFS/13_섬나라아일랜드.py
@@ -0,0 +1,28 @@
+import os
+path = os.path.realpath(__file__)
+path = path[0:path.rfind("/")]
+import sys
+from collections import deque
+sys.stdin=open(path+"/input.txt", "rt")
+
+dx=[-1, -1, 0, 1, 1, 1, 0, -1]
+dy=[0, 1, 1, 1, 0, -1, -1, -1]
+n=int(input())
+board=[list(map(int, input().split())) for _ in range(n)]
+cnt=0
+Q=deque()
+for i in range(n):
+    for j in range(n):
+        if board[i][j]==1:
+            board[i][j]=0
+            Q.append((i, j))
+            while Q:
+                tmp=Q.popleft()
+                for k in range(8):
+                    x=tmp[0]+dx[k]
+                    y=tmp[1]+dy[k]
+                    if 0<=x<n and 0<=y<n and board[x][y]==1:
+                        board[x][y]=0
+                        Q.append((x, y))
+            cnt+=1
+print(cnt)
