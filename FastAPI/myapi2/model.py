e67e2fc8327a076aa913522be5d1c2b44eb37731
pws0601 <ws0601@naver.com>
2023-05-16 20:01:50 +0900

    FastAPI 테스트 코드 작성

---
+++ FastAPI/myapi2/model.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    item: str
+    status: str
+
+class Todo(BaseModel):
+    id: int
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example" : {
+                "id" : 1,
+                "item" : "Example Schema!"
+            }
+        }
+
+class TodoItem(BaseModel):    
+    item: str
+    class Config:
+        schema_extra = {
+            "example" : {                
+                "item" : "Example Schema!"
+            }
+        }
