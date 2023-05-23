5b38cdfe67c80084d9999866b80be16e5b9e9dca
pws0601 <ws0601@naver.com>
2023-05-24 00:19:37 +0900

    20230524 flutter 스터디 코드

---
+++ flutter_workspace/video_call/lib/const/agora.dart
@@ -0,0 +1,6 @@
+const APP_ID = '070144cc08bd4c22babc39b232227112';
+
+const CHANNEL_NAME = 'testchannel';
+
+const TEMP_TOKEN = '007eJxTYCi1fb4r/oVjWJ2+/LHqnjr3R7tLeIof6658uofXUCU+PlKBwcDcwNDEJDnZwCIpxSTZyCgpMSnZ2DLJyNjIyMjc0NDoxomclIZARgb/ibUMjFAI4nMzlKQWlyRnJOblpeYwMAAAALEhhw==';
+
