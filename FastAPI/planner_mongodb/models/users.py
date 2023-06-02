b0eae048a0a4535fda34815d626d953d03754778
wooseok park <pws0601@hyosung.com>
2023-06-02 16:59:06 +0900

    FastAPI 테스트 코드 업데이트

---
+++ FastAPI/planner_mongodb/models/users.py
@@ -0,0 +1,31 @@
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+class User(Document):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
+        }
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }
